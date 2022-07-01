acd287c4b9de1084cadcbc2e4fb42bf2e37bf7f3
Riwaj Mainali <riwaj227@gmail.com>
2022-06-30 21:18:10 -0500

    Basic login UI
    gotta add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  class Login extends React.Component{
+    render(){
+      //popup
+      return(
+        <div>
+          <div className="login-popup">
+            <div className="login-popup-content">
+              <div className="login-popup-header">
+                <h1>Login</h1>
+              </div>
+              <div className="login-popup-body">
+                <div className="login-popup-body-input">
+
+                  <input type="text" placeholder="Username" />
+
+                  <button>Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+  class Navbar extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Navbar</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li onClick={()=>{root.render(<Login/>)}}>Login</li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+root.render(<Navbar />);
+}
+
 
 export default App;
