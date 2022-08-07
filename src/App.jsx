ba378bae528f8221dcd1d615e6e029142c2ce349
Riwaj Mainali <riwaj227@gmail.com>
2022-08-07 13:55:15 -0500

    Login functionality 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, 
-         signInWithEmailAndPassword,
-         onAuthStateChanged, 
-         signOut,
-} from 'firebase/auth';
-import {auth} from './firebase-config'
+
 import Input from "./components/Input";
 import Timer from "./components/Timer";
 import Stats from "./components/Stats";
 import { Accessibility } from "accessibility/src/main";
 import Popup from "./components/Popup/Popup";
+import Chart from "./components/Chart";
+import Login from "./components/Account/Login";
+import Dashboard from "./components/Account/Dashboard";
+import Register from "./components/Account/Register";
+import Reset from "./components/Account/Reset";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 window.addEventListener(
   "load",
   function () {
@@ -24,19 +25,6 @@ function App() {
   const [stats, setStats] = useState([]);
   const [modalIsOpen, modalToggle] = useState(false);
   const [isShowLogin, setIsShowLogin] = useState(false);
-  const [Login, setLogin] = useState([]);
-  const [loginTemp, setLoginTemp] = useState([]);
-  const [isShowRegister, setIsShowRegister] = useState(false);
-  const [Register, setRegister] = useState([]);
-  const [registerTemp, setRegisterTemp] = useState([]);
-
-  /*
-  const [user, setUser] = useState({});
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
-*/
-
 
   const startCountdown = async () => {
     for (let i = totalTime - 1; i >= 0; i--) {
@@ -63,114 +51,20 @@ function App() {
     e.preventDefault();
   };
 
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  const register = async () => {
-    try {
-    const user = await createUserWithEmailAndPassword(
-      auth,
-      registerEmail,
-      registerPassword
-    );
-    console.log(user)
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      console.log(user)
-      } catch (error) {
-        console.log(error.message);
-      }
-  };
-
-  const logout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <>
       <div className="mainDiv">
         <div className="outer-Login">
-          <button className="button-23" onClick={() => setIsShowLogin(true)}>
-            {" "}
-            login
+          <button
+            className="button-23"
+            onClick={() => setIsShowLogin(true)}
+            alt="login popup button"
+          >
+            Account
           </button>
         </div>
-        <Popup Trigger={isShowLogin} setTrigger={setIsShowLogin}>
-          <form onSubmit={handleSubmit} className="loginForm">
-            <label className="username">
-              <input
-                type="text"
-                placeholder="username"
-                onChange={(event) => {
-                  setLoginEmail(event.target.value);
-                }}
-              />
-            </label>
-            <br />
-            <label className="password">
-              <input type="password" 
-              placeholder="password" 
-              onChange={(event) => {
-                setLoginPassword(event.target.value);
-              }}
-              />
-            </label>
-
-            <button onClick={login} type="submit" className="button-23">
-              login
-            </button>
-          </form>
-        </Popup>
-        <div className="outer-Register">
-          <button className="button-23" onClick={() => setIsShowRegister(true)}>
-            {" "}
-            register
-          </button>
-        </div>
-        <Popup Trigger={isShowRegister} setTrigger={setIsShowRegister}>
-          <form onSubmit={handleSubmit} className="registerForm">
-            <label className="username">
-              <input
-                type="text"
-                placeholder="username"
-                onChange={(event) => {
-                  setRegisterEmail(event.target.value);
-                }}
-              />
-            </label>
-            <br />
-            <label className="password">
-              <input type="password" 
-              placeholder="password" 
-              onChange={(event) => {
-                setRegisterPassword(event.target.value);
-              }}
-              />
-            </label>
-            <button onClick={register} type="submit" className="button-23">
-              register
-            </button>
-          </form>
-        </Popup>
-
-        <h4> User Logged In: </h4>
-        
-        <button onClick={logout} type="submit" className="button-23">
-              Sign out
-            </button>
-        <h2 style={{ textAlign: "center" }}>Test your typing skills :)</h2>
+        <Popup Trigger={isShowLogin} setTrigger={setIsShowLogin}></Popup>
+        <h2 style={{ textAlign: "center" }}>Test your typing skills </h2>
         <Timer>{time}</Timer>
         <Input
           signalStart={() => {
@@ -196,6 +90,5 @@ function App() {
     </>
   );
 }
-
 
 export default App;
--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Login from "../Account/Login";
+import Dashboard from "../Account/Dashboard";
+import Register from "../Account/Register";
+import Reset from "../Account/Reset";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./Popup.css";
 export default function Popup(props) {
   return props.Trigger ? (
     <>
       <div className="Popup">
         <div className="Popup-inner">
-          <h1>Login</h1>
           <button
             className="button-23  close-btn"
             onClick={() => props.setTrigger(false)}
           >
             Close
           </button>
-          {props.children}
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/reset" element={<Reset />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </Router>
         </div>
       </div>
     </>
